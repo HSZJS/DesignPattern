b9ba6ec3046c0cac23071b4f1d3a1796e4a94f9b
HSZJS <xhdhjava@foxmail.com>
2019-10-18 00:51:02 +0800

    design_pattern_duck_demo

---
+++ src/test/java/headerFirst.designPattern/Tester.java
@@ -0,0 +1,15 @@
+package headerFirst.designPattern;
+
+import org.junit.Test;
+
+public class Tester {
+    /**
+     * goal:test duck class and method
+     * example: input null
+     * */
+    @Test
+    public void DuckTester(){
+     Duck duck = new Duck();
+
+    }
+}
