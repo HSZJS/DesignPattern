985fd44d71eb6af4f62f5dc13c141d7654c00e82
HSZJS <xhdhjava@foxmail.com>
2019-10-21 00:32:21 +0800

    currentConditionDisplay

--- src/main/java/com/headeFirst/designPattern/ObserverPattern/CurrentConditionsDisplay.java
+++ src/main/java/com/headeFirst/designPattern/ObserverPattern/CurrentConditionsDisplay.java
@@ -1,11 +1,23 @@
 package com.headeFirst.designPattern.ObserverPattern;
 
 public class CurrentConditionsDisplay implements Observer,DisplayElement {
+    private float temp;
+    private float humidity;
+    private Subject weatherData;
+    public CurrentConditionsDisplay(Subject weatherData){
+     this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
     public void update(float temp,float humidity,float pressure){
+        this.temp = temp;
+        this.humidity = humidity;
+        display();
         System.out.println("CurrentConditionsUpdate");
     }
     //display currency weater value
     public void display(){
+        System.out.println("current"+temp+humidity);
         System.out.println("CurrentConditionsDisplay");
     }
 }
