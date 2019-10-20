698429234ba72511b1edfb7e98ffa493edd27282
HSZJS <xhdhjava@foxmail.com>
2019-10-21 00:52:39 +0800

    test

--- src/test/java/headerFirst.designPattern/Tester.java
+++ src/test/java/headerFirst.designPattern/Tester.java
@@ -2,6 +2,8 @@ package headerFirst.designPattern;
 import com.headeFirst.designPattern.*;
 import org.junit.Test;
 
+import com.headeFirst.designPattern.ObserverPattern.*;
+
 public class Tester {
     /**
      * goal:test duck class and method
@@ -27,5 +29,12 @@ public class Tester {
         duck.swim();
         duck.display();
         duck.performFly();
+    }
+
+    @Test
+    public void WeaterStation(){
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionsDisplay current = new CurrentConditionsDisplay(weatherData);
+        weatherData.setMeasurements(80,100,30.4f);
     }
 }
